25da193bfc04b88f2f66e9dc7cde6587cb7eb183
mikrokontrolerpl <98092707+mikrokontrolerpl@users.noreply.github.com>
2022-01-21 13:40:49 +0100

    Update update.py

--- code/update.py
+++ code/update.py
@@ -1,5 +1,16 @@
 import trelloParsers
 
+import sys
+import os
+picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+from waveshare_epd import epd7in5_V2
+import time
+from PIL import Image,ImageDraw,ImageFont
+
 f = open('keys.dat')
 KEY = str(f.readline()).strip()
 TOKEN = str(f.readline()).strip()
@@ -23,3 +34,16 @@ file.close()
 
 print (lista)
 print (readlist)
+
+epd = epd7in5_V2.EPD()
+epd.init()
+epd.Clear()
+
+font35 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 35)
+
+
+Limage = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
+draw = ImageDraw.Draw(Limage)
+draw.text((2, 0), 'hello world', font = font35, fill = 0)
+epd.display(epd.getbuffer(Limage))
+epd.sleep()
